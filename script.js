7a037b1015f269fe35624f334fd37ff6eaa33821
mangeshahireakaXylus11 <97844580+xylus11@users.noreply.github.com>
2024-01-27 23:13:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function insert_Row() {
     //Write your code here
+	const newRow = document.getElementById("sampleTable");
+	newRow.document.innerHTML="<tr><td>Row1 cell1</td> 
+		<td>Row1 cell2</td></tr>";
+	
   
   
 }
